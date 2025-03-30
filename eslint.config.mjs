6b15bdb5bf826be79acc816197f2d523ac965872
Zygimantas Grinis <zgrinis@gmail.com>
2025-03-30 16:20:47 +0300

    connect eslint with prettier and setup pre-commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,23 +3,28 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
   tseslint.configs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
-    settings:{
-      react:{
-        version:'detect'
-      }
-    }
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
-  globalIgnores([
-    "dist",
-    "node_modules"
-  ])
+  eslintConfigPrettier,
+  globalIgnores(["dist", "node_modules"]),
 ]);
