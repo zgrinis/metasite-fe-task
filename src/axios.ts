ae79eea1d21a2091ff914a06d9bb1e2dafa3cd51
Zygimantas Grinis <zgrinis@gmail.com>
2025-04-07 20:12:01 +0300

    fallback rest api address

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const VITE_API_ADDRESS =
+  import.meta.env.VITE_API_ADDRESS ??
+  "https://frontend-task-api.metasite.lt/api";
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_ADDRESS,
+  baseURL: VITE_API_ADDRESS,
   headers: {
     "Content-Type": "application/json",
   },
